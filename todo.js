bb9768cf249456c9d13254c77e7562dedcffb99d
jhseob96 <jhseob96@gmail.com>
2023-03-05 19:16:49 +0900

    Todo list js file

---
+++ todo.js
@@ -0,0 +1,23 @@
+const toDoForm = document.querySelector(".toDoForm");
+const toDoInput = toDoForm.querySelector("input");
+const toDos = document.querySelector(".toDos");
+
+function paintToDo(toDo) {
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    span.innerHTML = toDo;
+    li.appendChild(span);
+    toDos.appendChild(li);
+}
+
+function createToDo(event) {
+    event.preventDefault();
+    const toDo = toDoInput.value;
+    paintToDo(toDo);
+    toDoInput.value = "";
+}
+
+function init() {
+    toDoForm.addEventListener("submit", createToDo);
+}
+init();
